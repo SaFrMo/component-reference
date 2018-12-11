8559613099fed3cc20893699459aaef4c4188067
Sander <sander@funkhaus.us>
2018-12-11 15:43:44 -0500

    Initial

---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <main class="layout">
+
+        <Content/>
+
+    </main>
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import './styles/base';
+.layout {
+}
+</style>
